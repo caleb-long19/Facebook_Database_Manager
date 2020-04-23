c222b4fba095e92fd0cd9aeb6c2d77c9039b44e6
caleb-long19 <caleblong199@gmail.com>
2020-04-23 17:07:04 +0100

    Database Application - Update 1 - Text Box Data
    Added a chunk of code which allows the data within the DataGrid to be displayed in Text Boxes. This will be used to update the data in the database!

--- FacebookDatabase/DatabaseTestingForm.Designer.cs
+++ FacebookDatabase/DatabaseTestingForm.Designer.cs
@@ -43,12 +43,7 @@
             this.label6 = new System.Windows.Forms.Label();
             this.txtHometown = new System.Windows.Forms.TextBox();
             this.txtCity = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.txtUniversity = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.txtWorkplace = new System.Windows.Forms.TextBox();
             this.btnExit = new System.Windows.Forms.Button();
-            this.btnAddUser = new System.Windows.Forms.Button();
             this.btnUpdateUser = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnDelete = new System.Windows.Forms.Button();
@@ -61,10 +56,11 @@
             this.databaseGridView.BackgroundColor = System.Drawing.Color.WhiteSmoke;
             this.databaseGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.databaseGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.databaseGridView.Location = new System.Drawing.Point(773, 107);
+            this.databaseGridView.Location = new System.Drawing.Point(637, 107);
             this.databaseGridView.Name = "databaseGridView";
             this.databaseGridView.Size = new System.Drawing.Size(512, 442);
             this.databaseGridView.TabIndex = 2;
+            this.databaseGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.databaseGridView_CellClick_1);
             // 
             // mainFormComboBox
             // 
@@ -74,10 +70,11 @@
             this.mainFormComboBox.Items.AddRange(new object[] {
             "Facebook Users",
             "Facebook Friends",
+            "Facebook Friends List",
             "Messages",
             "Universities",
             "Workplace"});
-            this.mainFormComboBox.Location = new System.Drawing.Point(773, 558);
+            this.mainFormComboBox.Location = new System.Drawing.Point(637, 558);
             this.mainFormComboBox.Name = "mainFormComboBox";
             this.mainFormComboBox.Size = new System.Drawing.Size(512, 28);
             this.mainFormComboBox.TabIndex = 3;
@@ -85,7 +82,7 @@
             // 
             // txtUserID
             // 
-            this.txtUserID.Location = new System.Drawing.Point(95, 133);
+            this.txtUserID.Location = new System.Drawing.Point(91, 174);
             this.txtUserID.Name = "txtUserID";
             this.txtUserID.Size = new System.Drawing.Size(175, 20);
             this.txtUserID.TabIndex = 4;
@@ -94,7 +91,8 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(21, 133);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(17, 174);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(68, 20);
             this.label1.TabIndex = 5;
@@ -104,7 +102,8 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(21, 171);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(17, 206);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(90, 20);
             this.label2.TabIndex = 6;
@@ -114,7 +113,8 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(21, 207);
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(17, 242);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(90, 20);
             this.label3.TabIndex = 7;
@@ -122,14 +122,14 @@
             // 
             // txtFirstName
             // 
-            this.txtFirstName.Location = new System.Drawing.Point(117, 173);
+            this.txtFirstName.Location = new System.Drawing.Point(113, 208);
             this.txtFirstName.Name = "txtFirstName";
             this.txtFirstName.Size = new System.Drawing.Size(153, 20);
             this.txtFirstName.TabIndex = 8;
             // 
             // txtLastName
             // 
-            this.txtLastName.Location = new System.Drawing.Point(117, 207);
+            this.txtLastName.Location = new System.Drawing.Point(113, 242);
             this.txtLastName.Name = "txtLastName";
             this.txtLastName.Size = new System.Drawing.Size(153, 20);
             this.txtLastName.TabIndex = 9;
@@ -138,7 +138,8 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(22, 243);
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(18, 278);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(67, 20);
             this.label4.TabIndex = 10;
@@ -146,7 +147,7 @@
             // 
             // txtGender
             // 
-            this.txtGender.Location = new System.Drawing.Point(95, 243);
+            this.txtGender.Location = new System.Drawing.Point(91, 278);
             this.txtGender.Name = "txtGender";
             this.txtGender.Size = new System.Drawing.Size(175, 20);
             this.txtGender.TabIndex = 11;
@@ -155,7 +156,8 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(22, 278);
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(18, 313);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(90, 20);
             this.label5.TabIndex = 12;
@@ -165,7 +167,8 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(22, 315);
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(18, 350);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(81, 20);
             this.label6.TabIndex = 14;
@@ -173,55 +176,22 @@
             // 
             // txtHometown
             // 
-            this.txtHometown.Location = new System.Drawing.Point(118, 280);
+            this.txtHometown.Location = new System.Drawing.Point(114, 315);
             this.txtHometown.Name = "txtHometown";
             this.txtHometown.Size = new System.Drawing.Size(279, 20);
             this.txtHometown.TabIndex = 13;
             // 
             // txtCity
             // 
-            this.txtCity.Location = new System.Drawing.Point(109, 315);
+            this.txtCity.Location = new System.Drawing.Point(105, 350);
             this.txtCity.Name = "txtCity";
             this.txtCity.Size = new System.Drawing.Size(288, 20);
             this.txtCity.TabIndex = 15;
             // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(22, 353);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(81, 20);
-            this.label7.TabIndex = 16;
-            this.label7.Text = "University:";
-            // 
-            // txtUniversity
-            // 
-            this.txtUniversity.Location = new System.Drawing.Point(109, 353);
-            this.txtUniversity.Name = "txtUniversity";
-            this.txtUniversity.Size = new System.Drawing.Size(288, 20);
-            this.txtUniversity.TabIndex = 17;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(21, 392);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(88, 20);
-            this.label8.TabIndex = 18;
-            this.label8.Text = "Workplace:";
-            // 
-            // txtWorkplace
-            // 
-            this.txtWorkplace.Location = new System.Drawing.Point(115, 392);
-            this.txtWorkplace.Name = "txtWorkplace";
-            this.txtWorkplace.Size = new System.Drawing.Size(282, 20);
-            this.txtWorkplace.TabIndex = 19;
-            // 
             // btnExit
             // 
             this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.Black;
             this.btnExit.Location = new System.Drawing.Point(18, 553);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(141, 37);
@@ -230,29 +200,17 @@
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
-            // btnAddUser
-            // 
-            this.btnAddUser.BackColor = System.Drawing.Color.Transparent;
-            this.btnAddUser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnAddUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddUser.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnAddUser.Location = new System.Drawing.Point(18, 439);
-            this.btnAddUser.Name = "btnAddUser";
-            this.btnAddUser.Size = new System.Drawing.Size(121, 34);
-            this.btnAddUser.TabIndex = 21;
-            this.btnAddUser.Text = "Add User";
-            this.btnAddUser.UseVisualStyleBackColor = false;
-            // 
             // btnUpdateUser
             // 
             this.btnUpdateUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnUpdateUser.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnUpdateUser.Location = new System.Drawing.Point(149, 439);
+            this.btnUpdateUser.Location = new System.Drawing.Point(22, 385);
             this.btnUpdateUser.Name = "btnUpdateUser";
             this.btnUpdateUser.Size = new System.Drawing.Size(121, 34);
             this.btnUpdateUser.TabIndex = 22;
             this.btnUpdateUser.Text = "Update User";
             this.btnUpdateUser.UseVisualStyleBackColor = true;
+            this.btnUpdateUser.Click += new System.EventHandler(this.btnUpdateUser_Click);
             // 
             // pictureBox1
             // 
@@ -261,7 +219,7 @@
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(12, 10);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1273, 91);
+            this.pictureBox1.Size = new System.Drawing.Size(1137, 91);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 23;
             this.pictureBox1.TabStop = false;
@@ -270,7 +228,7 @@
             // 
             this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDelete.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnDelete.Location = new System.Drawing.Point(276, 439);
+            this.btnDelete.Location = new System.Drawing.Point(149, 385);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(121, 34);
             this.btnDelete.TabIndex = 24;
@@ -282,16 +240,11 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SteelBlue;
-            this.ClientSize = new System.Drawing.Size(1297, 602);
+            this.ClientSize = new System.Drawing.Size(1161, 602);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnUpdateUser);
-            this.Controls.Add(this.btnAddUser);
             this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.txtWorkplace);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.txtUniversity);
-            this.Controls.Add(this.label7);
             this.Controls.Add(this.txtCity);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.txtHometown);
@@ -306,6 +259,7 @@
             this.Controls.Add(this.txtUserID);
             this.Controls.Add(this.mainFormComboBox);
             this.Controls.Add(this.databaseGridView);
+            this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "FacebookTestingForm";
             this.Text = "U";
@@ -331,12 +285,7 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox txtHometown;
         private System.Windows.Forms.TextBox txtCity;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox txtUniversity;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox txtWorkplace;
         private System.Windows.Forms.Button btnExit;
-        private System.Windows.Forms.Button btnAddUser;
         private System.Windows.Forms.Button btnUpdateUser;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btnDelete;
--- FacebookDatabase/DatabaseTestingForm.cs
+++ FacebookDatabase/DatabaseTestingForm.cs
@@ -22,6 +22,8 @@ namespace FacebookDatabase
             frmkeepFacebookTestingForm = this;
         }
 
+        int IndexRow;
+
         #region Allow User to select what Data displays in the Datagrid
         private void mainFormComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -62,7 +64,7 @@ namespace FacebookDatabase
                 using (MySqlConnection connection =
                     new MySqlConnection(connectionString))
                 {
-                    string query = "SELECT * FROM isad157_clong.facebook_friendships_listed_id";
+                    string query = "SELECT * FROM isad157_clong.facebook_friendships";
                     connection.Open();
                     MySqlCommand cmd = new MySqlCommand(query, connection);
 
@@ -75,8 +77,33 @@ namespace FacebookDatabase
             }
             #endregion
 
+            #region Display Facebook Friends List in Datagrid
+            if (mainFormComboBox.SelectedIndex == 2)
+            {
+                string connectionString = "SERVER=" + DBConnect.SERVER + ";" +
+                    "DATABASE=" + DBConnect.DATABASE_NAME + ";" + "UID=" +
+                    DBConnect.USER_NAME + ";" + "PASSWORD=" +
+                    DBConnect.PASSWORD + ";" + "SslMode=" +
+                    DBConnect.SslMode + ";";
+
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    string query = "SELECT * FROM isad157_clong.facebook_friends_list";
+                    connection.Open();
+
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    MySqlDataAdapter sqlDA = new MySqlDataAdapter(cmd);
+                    DataTable friendListDataTable = new DataTable();
+                    sqlDA.Fill(friendListDataTable);
+
+                    databaseGridView.DataSource = friendListDataTable;
+
+                } // End of using (MySqlConnection connection = ...
+            }
+            #endregion
+
             #region Display Facebook Messages in Datagrid
-            else if (mainFormComboBox.SelectedIndex == 2)
+            else if (mainFormComboBox.SelectedIndex == 3)
             {
                 string connectionString = "SERVER=" + DBConnect.SERVER + ";" +
                     "DATABASE=" + DBConnect.DATABASE_NAME + ";" + "UID=" +
@@ -87,7 +114,7 @@ namespace FacebookDatabase
                 using (MySqlConnection connection =
                     new MySqlConnection(connectionString))
                 {
-                    string query = "SELECT * FROM isad157_clong.facebook_messages_listed_id";
+                    string query = "SELECT * FROM isad157_clong.facebook_messages";
                     connection.Open();
                     MySqlCommand cmd = new MySqlCommand(query, connection);
 
@@ -101,7 +128,7 @@ namespace FacebookDatabase
             #endregion
 
             #region Display Facebook Users Universities in Datagrid
-            else if (mainFormComboBox.SelectedIndex == 3)
+            else if (mainFormComboBox.SelectedIndex == 4)
             {
                 string connectionString = "SERVER=" + DBConnect.SERVER + ";" +
                     "DATABASE=" + DBConnect.DATABASE_NAME + ";" + "UID=" +
@@ -112,7 +139,7 @@ namespace FacebookDatabase
                 using (MySqlConnection connection =
                     new MySqlConnection(connectionString))
                 {
-                    string query = "SELECT * FROM isad157_clong.facebook_universities_listed_id";
+                    string query = "SELECT * FROM isad157_clong.facebook_universities";
                     connection.Open();
                     MySqlCommand cmd = new MySqlCommand(query, connection);
 
@@ -126,7 +153,7 @@ namespace FacebookDatabase
             #endregion
 
             #region Display Facebook User Workplaces in Datagrid
-            else
+            else if (mainFormComboBox.SelectedIndex == 5)
             {
                 string connectionString = "SERVER=" + DBConnect.SERVER + ";" +
                     "DATABASE=" + DBConnect.DATABASE_NAME + ";" + "UID=" +
@@ -137,7 +164,7 @@ namespace FacebookDatabase
                 using (MySqlConnection connection =
                     new MySqlConnection(connectionString))
                 {
-                    string query = "SELECT * FROM isad157_clong.workplace_listed_id";
+                    string query = "SELECT * FROM isad157_clong.facebook_workplace";
                     connection.Open();
                     MySqlCommand cmd = new MySqlCommand(query, connection);
 
@@ -152,6 +179,41 @@ namespace FacebookDatabase
         }
 
 
+        #endregion
+
+        #region Used to Display the Data from the Datagrid into the Text box
+        private void databaseGridView_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+            IndexRow = e.RowIndex;
+
+            if (mainFormComboBox.SelectedIndex == 0)
+            {
+                if (IndexRow >= 0)
+                {
+                    DataGridViewRow row = this.databaseGridView.Rows[IndexRow];
+
+                    txtUserID.Text = row.Cells["UserID"].Value.ToString();
+                    txtFirstName.Text = row.Cells["FirstName"].Value.ToString();
+                    txtLastName.Text = row.Cells["LastName"].Value.ToString();
+                    txtGender.Text = row.Cells["Gender"].Value.ToString();
+                    txtHometown.Text = row.Cells["Hometown"].Value.ToString();
+                    txtCity.Text = row.Cells["City"].Value.ToString();
+                }
+            }
+        }
+        #endregion
+
+        #region Used to Update data in the Database
+        private void btnUpdateUser_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow updateDataRow = databaseGridView.Rows[IndexRow];
+            updateDataRow.Cells[0].Value = txtFirstName.Text;
+            updateDataRow.Cells[1].Value = txtLastName.Text;
+            updateDataRow.Cells[2].Value = txtGender.Text;
+            updateDataRow.Cells[3].Value = txtHometown.Text;
+            updateDataRow.Cells[4].Value = txtCity.Text;
+        }
         #endregion
 
         #region Button Functionality on the Main Form
